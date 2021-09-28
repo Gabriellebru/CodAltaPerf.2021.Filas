5f9d1bf36f998dc3caf7d3846258cb01176d836f
gabrielle <gabriellebruna2011@hotmail.com>
2021-09-28 11:25:09 -0300

    Adicionar arquivos de projeto.

---
+++ CodAltaPerf.2021.Filas/Program.cs
@@ -0,0 +1,115 @@
+﻿using CodAltaPerf._2021.Filas.Entidades;
+using CodAltaPerf._2021.Filas.Util;
+using System;
+
+namespace CodAltaPerf._2021.Filas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //campo de variaveis
+            bool ManterAppAberto = true;
+            int MenuUsuarioOption;
+
+            FilaEstaticaInteiros FilaNormal = new FilaEstaticaInteiros();
+            FilaEstaticaInteiros FilaPreferencial = new FilaEstaticaInteiros();
+
+
+            //codigo da aplicação
+            while (ManterAppAberto)
+            {
+                Uteis.MenuUsuario();
+                MenuUsuarioOption = Uteis.PegaOpçãoDigitada();
+
+                switch (MenuUsuarioOption)
+                {
+                    case 1:
+                        //usuario caixa
+                        bool ManterMenuCaixaAberto = true;
+                        int MenuCaixaOption;
+                        int chamada;
+
+                        while (ManterMenuCaixaAberto)
+                        {
+                            Uteis.MenuCaixa();
+                            MenuCaixaOption = Uteis.PegaOpçãoDigitada();
+
+                            switch (MenuCaixaOption)
+                            {
+                                case 1:
+                                    // caixa comum
+                                    if(FilaNormal.Tamanho() > 0)
+                                    {
+                                        Uteis.Chamada(1, FilaNormal.Tamanho());
+                                        FilaNormal.Desenfilera();
+                                    }
+                                    else
+                                    {
+                                        Uteis.ErroChamada(1);
+                                    }
+                                    break;
+                                case 2:
+                                    // caixa prioritario
+                                    if (FilaPreferencial.Tamanho() > 0)
+                                    {
+                                        Uteis.Chamada(2, FilaPreferencial.Tamanho());
+                                        FilaPreferencial.Desenfilera();
+                                    }
+                                    else
+                                    {
+                                        Uteis.ErroChamada(2);
+                                    }
+                                    break;
+                                case 0:
+                                    ManterMenuCaixaAberto = false;
+                                    break;
+                                default:
+                                    Uteis.InvalidOptionError();
+                                    break;
+                            }
+                        }
+
+                        break;
+
+
+                    // READY
+                    case 2:
+                        bool ManterMenuSenhasAberto = true;
+                        int MenuSenhasOption;
+                        while (ManterMenuSenhasAberto)
+                        {
+                            Uteis.MenuSenhas();
+                            MenuSenhasOption = Uteis.PegaOpçãoDigitada();
+
+                            switch (MenuSenhasOption)
+                            {
+                                case 1:
+                                    FilaNormal.Enfilera(FilaNormal.Tamanho() + 1);
+                                    Uteis.Chamada(1, FilaNormal.Tamanho());
+                                    break;
+                                case 2:
+                                    FilaPreferencial.Enfilera(FilaPreferencial.Tamanho() + 1);
+                                    Uteis.Chamada(2, FilaPreferencial.Tamanho());
+                                    break;
+                                case 0:
+                                    ManterMenuSenhasAberto = false;
+                                    break;
+                                default:
+                                    Uteis.InvalidOptionError();
+                                    break;
+                            }
+                        }
+                        break;
+                    case 0:
+                        ManterAppAberto = false;
+                        break;
+                    default:
+                        Uteis.InvalidOptionError();
+                        // opção invalida
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ CodAltaPerf.2021.Filas/Util/Uteis.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodAltaPerf._2021.Filas.Util
+{
+    public static class Uteis
+    {
+        public static void MenuUsuario()
+        {
+            Console.Clear();
+            Console.WriteLine("Escolha seu usuário\n");
+            Console.WriteLine("1- Usuario caixa");
+            Console.WriteLine("2- Usuário cliente");
+            Console.WriteLine("0- Sair\n");
+            Console.Write("Digite sua escolha: ");
+        }
+        public static void MenuCaixa()
+        {
+            Console.Clear();
+            Console.WriteLine("Escolha seu caixa\n");
+            Console.WriteLine("1- Caixa comum");
+            Console.WriteLine("2- Caixa prioritário");
+            Console.WriteLine("0- Voltar ao menu anterior\n");
+            Console.Write("Digite sua escolha: ");
+        }
+
+        public static void MenuSenhas()
+        {
+            Console.Clear();
+            Console.WriteLine("Retire sua senha\n");
+            Console.WriteLine("1- Senha comum");
+            Console.WriteLine("2- Senha prioritária");
+            Console.WriteLine("0- Voltar ao menu anterior\n");
+            Console.Write("Digite sua escolha: ");
+        }
+
+        public static void InvalidOptionError()
+        {
+            Console.WriteLine("Opção inválida, tente novamente.\n");
+            Console.WriteLine("Pressione ENTER para continuar...");
+            Console.ReadLine();
+            Console.Clear();
+        }
+
+        public static int PegaOpçãoDigitada()
+        {
+            int option;
+            try
+            {
+                option = int.Parse(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                option = -1;
+            }
+            return option;
+        } 
+
+        public static void Chamada(int tipo, int posicao)
+        {
+            switch (tipo)
+            {
+                case 1: // normal
+                    Console.WriteLine($"CXN-{posicao.ToString("000")}");
+                    break;
+                case 2: // preferencial
+                    Console.WriteLine($"CXP-{posicao.ToString("000")}");
+                    break;
+            }
+            Console.ReadLine();
+        }
+
+        public static void ErroChamada(int tipo)
+        {
+            switch (tipo)
+            {
+                case 1:
+                    Console.WriteLine("Fila Normal está vazia.");
+                    break;
+                case 2:
+                    Console.WriteLine("Fila Preferencial está vazia.");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
